a8b1b1814422cddaa21afef9b029047b7cd7caef
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2021-11-24 23:22:28 +0200

    Update tables_init.sql

--- tables_init.sql
+++ tables_init.sql
@@ -7,17 +7,25 @@ CREATE TABLE IF NOT EXISTS Staff (
 
 CREATE TABLE IF NOT EXISTS Customers (
     c_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(15),
-    last_name VARCHAR(15),
+    first_name VARCHAR(15) NOT NULL,
+    last_name VARCHAR(15) NOT NULL,
     phone_number int 
 );
 
 CREATE TABLE IF NOT EXISTS Reservations(
     r_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     c_id INT NOT NULL REFERENCES Customers(c_id),
-    date_of_reservation DATE,
-    date_of_arrival DATE ,
-    nights INT,
-    food TEXT CHECK (food IN('yes','no')),
     room_id INT NOT NULL REFERENCES Rooms(room_id),
+    date_of_reservation DATE NOT NULL,
+    date_of_arrival DATE NOT NULL,
+    nights INT NOT NULL,
+    food TEXT CHECK (food IN('yes','no')),
+);
+
+CREATE TABLE IF NOT EXISTS Rooms(
+    room_id INT AUTO_INCREMENT NOT NULL,
+    class TEXT CHECK (class IN('Luxury','Economy')),
+    beds INT,
+    availability TEXT CHECK (availability IN ('yes','no')),
+    room_number INT 
 );
