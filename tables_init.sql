10e40ed961f7ff1b888f3ecbd2ee1dfe5a546bd5
Haxemaniac <40740528+Haxemaniac@users.noreply.github.com>
2021-11-24 22:30:02 +0200

    Update tables_init.sql

--- tables_init.sql
+++ tables_init.sql
@@ -1,6 +1,18 @@
-CREATE TABLE Staff (
+CREATE TABLE IF NOT EXISTS Staff (
 	staff_id int NOT NULL auto_increment PRIMARY KEY,
 	job enum('cleaner','carrier','receptionist','manager','cook','waiter') NOT NULL DEFAULT 'waiter',
 	sex enum('male','female') DEFAULT 'male',
 	hiring_date DATE
+);
+
+CREATE TABLE IF NOT EXISTS Clients (
+    c_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name CHAR[15],
+    last_name CHAR[15],
+    phone_number int 
+);
+
+CREATE TABLE IF NOT EXISTS Reservations(
+    r_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    c_id INT NOT NULL FOREIGN KEY 
 );
